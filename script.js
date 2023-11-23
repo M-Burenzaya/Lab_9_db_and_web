fbf92961cf1381efffd5717c7ce97f0c852303b6
Burenzaya <bvrnee1009@gmail.com>
2023-11-23 23:07:29 +0800

    php

---
+++ script.js
@@ -0,0 +1,56 @@
+function addStudent() {
+    const formData = new FormData(document.getElementById("studentForm"));
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                window.alert("Data sent successfully: " + this.responseText);
+            } else {
+                window.alert("Error: " + this.statusText);
+            }
+        }
+    };
+
+    xhttp.open("POST", "student.php", true);
+    xhttp.send(formData);
+}
+
+function addLibraryVisit() {
+    const formData = new FormData(document.getElementById("visitForm"));
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                window.alert("Data sent successfully: " + this.responseText);
+            } else {
+                window.alert("Error: " + this.statusText);
+            }
+        }
+    };
+
+    xhttp.open("POST", "library.php", true);
+    xhttp.send(formData);
+}
+
+function showStudentHours() {
+    const formData = new FormData(document.getElementById("hoursForm"));
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                document.querySelector(".result").innerHTML = this.responseText;
+            } else {
+                window.alert("Error: " + this.statusText);
+            }
+        }
+    };
+
+    xhttp.open("POST", "join_student_visit.php", true);
+    xhttp.send(formData);
+}
