b3ce4385081d056e5679f655630ab6b21e52e658
Burenzaya <bvrnee1009@gmail.com>
2023-11-24 01:04:25 +0800

    responce

--- script.js
+++ script.js
@@ -1,62 +1,57 @@
 function addStudent() {
     const formData = new FormData(document.getElementById("studentForm"));
 
-    fetch("student.php", {
-        method: "POST",
-        body: formData,
-    })
-        .then((response) => {
-            if (response.ok) {
-                return response.text();
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                window.alert("Data sent successfully: " + this.responseText);
+                document.querySelector('#result').innerHTML = this.responseText;
+            } else {
+                window.alert("Error: " + this.statusText);
             }
-            throw new Error("Network response was not ok.");
-        })
-        .then((data) => {
-            window.alert("Data sent successfully: " + data);
-        })
-        .catch((error) => {
-            window.alert("Error: " + error.message);
-        });
+        }
+    };
+
+    xhttp.open("POST", "student.php", true);
+    xhttp.send(formData);
 }
 
 function addLibraryVisit() {
     const formData = new FormData(document.getElementById("visitForm"));
 
-    fetch("library.php", {
-        method: "POST",
-        body: formData,
-    })
-        .then((response) => {
-            if (response.ok) {
-                return response.text();
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                window.alert("Data sent successfully: " + this.responseText);
+            } else {
+                window.alert("Error: " + this.statusText);
             }
-            throw new Error("Network response was not ok.");
-        })
-        .then((data) => {
-            window.alert("Data sent successfully: " + data);
-        })
-        .catch((error) => {
-            window.alert("Error: " + error.message);
-        });
+        }
+    };
+
+    xhttp.open("POST", "visit.php", true);
+    xhttp.send(formData);
 }
 
 function showStudentHours() {
     const formData = new FormData(document.getElementById("hoursForm"));
 
-    fetch("join_student_visit.php", {
-        method: "POST",
-        body: formData,
-    })
-        .then((response) => {
-            if (response.ok) {
-                return response.text();
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                document.querySelector(".result").innerHTML = this.responseText;
+            } else {
+                window.alert("Error: " + this.statusText);
             }
-            throw new Error("Network response was not ok.");
-        })
-        .then((data) => {
-            document.querySelector(".result").innerHTML = data;
-        })
-        .catch((error) => {
-            window.alert("Error: " + error.message);
-        });
+        }
+    };
+
+    xhttp.open("POST", "join_student_visit.php", true);
+    xhttp.send(formData);
 }
--- student.php
+++ student.php
@@ -1,5 +1,17 @@
-<php?
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: *");
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $stud_id = $_POST["stud_id"] ?? null;
+    $lname = $_POST["lname"] ?? null;
+    $fname = $_POST["fname"] ?? null;
+
+    // Displaying received values (for testing purposes)
+    echo "Received data:<br>";
+    echo "Student ID: " . $stud_id . "<br>";
+    echo "Last Name: " . $lname . "<br>";
+    echo "First Name: " . $fname . "<br>";
+} else {
+    echo "Invalid request method";
+}
+
 ?>
