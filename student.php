ddde931244915803bc337c2121354c69ede73f45
Burenzaya <bvrnee1009@gmail.com>
2023-11-24 05:26:07 +0800

    student php

--- student.php
+++ student.php
@@ -1,17 +1,42 @@
 <?php
 
+$user = 'root';
+$pass = '';
+$dbName = 'lab_10';
+$host = 'localhost';
+
+$conn = new mysqli($host, $user, $pass, $dbName);
+
+if ($conn->connect_error) {
+    die("Холболт амжилтгүй: " . $conn->connect_error);
+}
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
     $stud_id = $_POST["stud_id"] ?? null;
     $lname = $_POST["lname"] ?? null;
     $fname = $_POST["fname"] ?? null;
 
-    // Displaying received values (for testing purposes)
-    echo "Received data:<br>";
-    echo "Student ID: " . $stud_id . "<br>";
-    echo "Last Name: " . $lname . "<br>";
-    echo "First Name: " . $fname . "<br>";
+    if ($stud_id !== null && $lname !== null && $fname !== null) {
+    
+        $stmt = $conn->prepare("INSERT INTO Students (StudentID, lname, fname) VALUES (?, ?, ?)");
+        $stmt->bind_param("iss", $stud_id, $lname, $fname);
+
+        $stmt->execute();
+
+        if ($stmt->affected_rows > 0) {
+            echo "Өгөгдөл амжилттай нэмэгдлээ.";
+        } else {
+            echo "Нэмэх үйлдэл амжилтгүй: " . $conn->error;
+        }
+
+        $stmt->close();
+    } else {
+        echo "Бүх талбарыг бөглөнө үү.";
+    }
 } else {
-    echo "Invalid request method";
+    echo "Зөвхөн POST хүсэлт ашиглана уу.";
 }
 
+$conn->close();
 ?>
